cf54453efa76e6b827922df59e3aeef9537930da
orkhanAbdulla <Orkhanaa@code.edu.az>
2022-09-16 15:47:10 +0400

    Homework change added

--- PYP-CustomMiddleware/Extentions/CustomMiddlewareExstentions.cs
+++ PYP-CustomMiddleware/Extentions/CustomMiddlewareExstentions.cs
@@ -1,6 +1,12 @@
-﻿namespace PYP_CustomMiddleware.Extentions
+﻿using PYP_CustomMiddleware.Middlewares;
+
+namespace PYP_CustomMiddleware.Extentions
 {
-    public class CustomMiddlewareExstention
+    public static class CustomMiddlewareExstentions
     {
+        public static IApplicationBuilder UseContent(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<CustomMiddleware>();
+        }
     }
 }
--- PYP-CustomMiddleware/Middlewares/CustomMiddleware.cs
+++ PYP-CustomMiddleware/Middlewares/CustomMiddleware.cs
@@ -1,6 +1,37 @@
-﻿namespace PYP_CustomMiddleware.Middlewares
+﻿using System.Collections.Generic;
+
+namespace PYP_CustomMiddleware.Middlewares
 {
-    public class CustomMiddlaware
+    public class CustomMiddleware
     {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public CustomMiddleware(IConfiguration configuration, RequestDelegate next)
+        {
+            _configuration = configuration;
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+           var CompanyDatas= _configuration.GetSection("CompanyInfo").GetChildren();
+           Dictionary<string,string> parameters = new Dictionary<string, string>();
+            foreach (var data in CompanyDatas)
+            {
+                parameters.Add(data.Key, data.Value);
+            }
+            context.Response.OnStarting(state =>
+            {
+                var httpContext = (HttpContext)state;
+                foreach (var keyValue in parameters)
+                {
+                    httpContext.Response.Headers.Add(keyValue.Key, keyValue.Value);
+                }
+                return Task.CompletedTask;
+            },context);
+            await _next(context);
+        }
+
+       
     }
 }
